a1043795a1bdeccaf875252df8e5a4fa27c39ce0
cristhianreyesp <cristhianreyesp04@gmail.com>
2025-01-29 23:56:08 -0500

    Guardando cambios

--- src/main/java/com/retotecnico/modelo/Animal.java
+++ src/main/java/com/retotecnico/modelo/Animal.java
@@ -2,4 +2,17 @@ package com.retotecnico.modelo;
 
 public abstract class Animal {
 
+    protected String nombre;
+    protected String sonido;
+
+    public Animal(String nombre, String sonido) {
+        this.nombre = nombre;
+        this.sonido = sonido;
+    }
+
+    public abstract String getType();
+
+    public void produceSonido() {
+        System.out.println(nombre + " hace: " + sonido);
+    }
 }
