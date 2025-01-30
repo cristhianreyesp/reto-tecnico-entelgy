8ca4467820153cffb5647563e1410ffe747a8fa4
cristhianreyesp <cristhianreyesp04@gmail.com>
2025-01-29 23:26:22 -0500

    Feat: Implementacion del del reto

--- src/main/java/com/retotecnico/config/ConfigAplicacion.java
+++ src/main/java/com/retotecnico/config/ConfigAplicacion.java
@@ -1,5 +1,14 @@
 package com.retotecnico.config;
 
+import com.retotecnico.servicio.AnimalServicio;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ConfigAplicacion {
 
+    @Bean
+    public AnimalServicio animalServicio() {
+        return new AnimalServicio();
+    }
 }
